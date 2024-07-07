6c49c261684ce53348b579221e44ff8a48680b0a
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-07-07 14:48:50 +0700

    Refine

--- src/App/homePage.tsx
+++ src/App/homePage.tsx
@@ -1,22 +1,11 @@
-import {useEffect, useState} from "react";
-import {getAllRooms} from "../utils/ApiHelperFunctions.ts";
-import { Room } from "../types"; // Import the Room interface
-
+import {useEffect} from "react";
 
 const HomePage = () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [test, setTest] = useState<Room[] | undefined[]>()
-
     useEffect(() => {
-        const fetchData = async () => {
-            const rooms = await getAllRooms();
-            setTest(rooms || []);
-        }
-        fetchData();
+
     }, []);
     return (
         <div>
-            {test ? test.at(0)?.roomNumber : "Loading..."}
         </div>
     )
 }
---
+++ src/components/room/romTypeSelector.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+
+}
+
+const RomTypeSelector = (props: Props) => {
+  return (
+      <div>home</div>
+  )
+}
+
+export default RomTypeSelector
